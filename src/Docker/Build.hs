9b93778b4c6660774b3edbfa14920512dc3e23fc
Vitor Roque <vitor.roquep@gmail.com>
2025-05-21 23:48:46 -0300

    fix: adding stuff for building

--- src/Docker/Build.hs
+++ src/Docker/Build.hs
@@ -2,3 +2,19 @@ module Docker.Build () where
 
 import Network.HTTP.Client
 import Network.HTTP.Client.Unix
+import Network.HTTP.Types.Header (hContentType)
+import qualified Data.ByteString.Lazy.Char8 as LBS
+import System.IO (stdout)
+import Control.Monad (void)
+
+tarPath :: FilePath
+tarPath = "context.tar"
+
+socketPath :: FilePath
+socketPath = "/var/run/docker.sock"
+
+imageName :: String
+imageName = "sentinel_image:latest"
+
+buildImage :: IO ()
+buildImage = undefined
