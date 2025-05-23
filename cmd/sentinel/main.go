9cbc83e3450a2b2490270d79f8b958cccab4b258
Vitor Roque <vitor.roquep@gmail.com>
2025-05-23 02:16:01 -0300

    fix: adding things

--- cmd/sentinel/main.go
+++ cmd/sentinel/main.go
@@ -2,70 +2,88 @@ package main
 
 import (
 	"archive/tar"
-	"io"
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
 )
 
-func compact_dir(srcDir string) ([]byte, error) {
-	var buf bytes.Buffer
-	tw := tar.NewWriter(&buf)
-
-	err := filepath.Walk(srcDir, func(file string, fi os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if fi.IsDir() {
-			return nil
-		}
-		f, err := os.Open(file)
-		if err != nil {
-			return err
-		}
-		defer f.Close()
-		relPath, err := filepath.Rel(srcDir, file)
-		if err != nil {
-			return err
-		}
-		relPath = filepath.ToSlash(relPath)
-		header, err := tar.FileInfoHeader(fi, relPath)
-		if err != nil {
-			return err
-		}
-		header.Name = relPath
-
-		if err := tw.WriteHeader(header); err != nil {
-			return err
-		}
-		if _, err := io.Copy(tw, f); err != nil {
-			return err
-		}
-
-		return nil
-	})
-	if err != nil {
-		return nil, err
-	}
-	if err := tw.Close(); err != nil {
-		return nil, err
-	}
-
-	return buf.Bytes(), nil
-}
-func main() {
+func build_file_info_header(tarWriter *tar.Writer, filename string, fileInfo os.FileInfo, err error) error {
 	currentDir, err := os.Getwd()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-	tarBytes, err := compact_dir(currentDir)
+
+	if fileInfo.IsDir() {
+		return nil
+	}
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	relativePath, err := filepath.Rel(currentDir, filename)
+
+	if err != nil {
+		return err
+	}
+
+	relativePath = filepath.ToSlash(relativePath)
+
+	header, err := tar.FileInfoHeader(fileInfo, relativePath)
+	if err != nil {
+		return err
+	}
+
+	header.Name = relativePath
+
+	if err := tarWriter.WriteHeader(header); err != nil {
+		return err
+	}
+	if _, err := io.Copy(tarWriter, file); err != nil {
+		return err
+	}
+	return nil
+}
+
+func compact_dir() ([]byte, error) {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	var buffer bytes.Buffer
+	tarWriter := tar.NewWriter(&buffer)
+
+	err = filepath.Walk(currentDir, func(filename string, fileInfo os.FileInfo, err error) error {
+		err = build_file_info_header(tarWriter, filename, fileInfo, err)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	if err := tarWriter.Close(); err != nil {
+		return nil, err
+	}
+
+	return buffer.Bytes(), nil
+}
+
+func main() {
+	tarBytes, err := compact_dir()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = os.WriteFile("context.tar",tarBytes, 0644)
+	err = os.WriteFile("context.tar", tarBytes, 0644)
 	if err != nil {
 		panic(err)
 	}
